2091ba85b595f5680c9ddf77e5962a0dbfc9aeb2
peterkaiserui <peterkaiserui@gmail.com>
2016-01-15 18:54:53 -0800

    [feat]: added server and user login/signup routes

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var db = require('./data.js');
+
+require('./config/middleware.js')(app,express);
+
+var port = process.env.PORT || 8080;
+
+app.set('port', port);
+
+app.get('/',function(req,res){
+    res.send('Hello World')
+
+})
+
+app.listen(port,function(){
+    console.log("Listening to port"+ port)
+});
