9e1d234ae66f24f57eb1ba0b34f00d56a1f85e5f
Peter Kaiser <peterkaiserui@gmail.com>
2016-01-15 19:02:45 -0800

    Update userRoutes.js

--- server/user/userRoutes.js
+++ server/user/userRoutes.js
@@ -1,7 +1,7 @@
 var userController = require('./userController.js');
 
 module.exports = function(app){
-    app.post('signup',userController.signup);
+    app.post('/signup',userController.signup);
     app.post('/signin', userController.signin);
     app.post('/signedin',userController.checkAuth);
 }
