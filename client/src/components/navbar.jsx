636de850b2a4f31a60b809914cf473a8fe14ce61
steele2345 <beresford211@users.noreply.github.com>
2016-01-16 16:12:16 -0800

    [feat]: add react navbar no css styling

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,22 @@
+var NavBar = React.createClass({
+  render: function() {
+    return (
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Chutte</a>
+            <ul class="nav nav-pills">
+              <li role="presentation"><a href="#">Sign Up</a></li>
+              <li role="presentation"><a href="#">Login In</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+});
+
+ReactDOM.render(
+  <NavBar/>,
+  document.getElementById('container')
+);
