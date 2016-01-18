26143bff9352eb740e8bd486bf797d814a5dcc40
seanagibson <seanandgibson@gmail.com>
2016-01-18 11:22:05 -0800

    [refactor]: adds routes to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
+import {syncHistory, routeReducer} from 'redux-simple-router';
 import App from './containers/app';
+import Browse from './containers/browse';
+import Dashboard from './containers/dashboard';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const reduxRouterMiddleware = syncHistory(browserHistory);
+
+const app = document.createElement('div');
+document.body.appendChild(app);
+
+ReactDOM.render(
+  <Provider>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <Route path="browse" component={Browse}/>
+        <Route path="dashboard" component={Dashboard}/>
+      </Route>
+    </Router>
+  </Provider>, app);
