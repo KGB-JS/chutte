5fb5516ad8c89a4a20015777f1f3f1714b133b16
Michael Berber <mickberber@icloud.com>
2016-01-18 11:54:38 -0800

    [fix]: fix data.js

--- server/data.js
+++ server/data.js
@@ -1,94 +1,83 @@
 module.exports = {
-    "token": "569972b272b68de1f5dd109f",
-    "userData": {
-        "username": "suzanne_378",
-        "email": "suzannemontgomery@koogle.com",
-        "firstname": "Roman",
-        "lastName": "Alford",
-        "phone": "+1 (851) 578-3368",
-        "address": "676 Richmond Street, Efland, South Carolina, 1369"
-    },
-    "data": {
-        "items": [{
-            "id": 1051645,
-            "productName": "Zillanet",
-            "quanity": 107,
-            "price": 88560,
-            "auctionEnds": "2015-11-20T09:31:29 +08:00",
-            "priceReduces": "2015-04-30T09:30:31 +07:00",
-            "description": "Enim est ipsum consequat deserunt proident est. Fugiat magna eiusmod fugiat ullamco labore qui culpa velit irure laboris laborum quis id officia. Sunt velit occaecat ea elit ipsum exercitation excepteur enim qui. Incididunt incididunt dolor in aute enim incididunt consectetur irure ipsum.\r\n"
-        }, {
-            "id": 1239391,
-            "productName": "Papricut",
-            "quanity": 32,
-            "price": 53540,
-            "auctionEnds": "2015-05-31T08:21:19 +07:00",
-            "priceReduces": "2016-01-12T07:54:01 +08:00",
-            "description": "In ad sit amet et. Commodo excepteur proident culpa elit irure fugiat proident ut consequat ut pariatur fugiat. Laborum reprehenderit anim sit qui elit est occaecat. Deserunt cillum tempor dolor eu. Excepteur exercitation qui dolor laboris in eu minim ullamco fugiat. Id aliqua non in nulla sit deserunt ipsum sit nisi aliquip sunt exercitation.\r\n"
-        }, {
-            "id": 38197,
-            "productName": "Buzzmaker",
-            "quanity": 59,
-            "price": 64990,
-            "auctionEnds": "2015-09-14T05:26:05 +07:00",
-            "priceReduces": "2015-06-24T08:32:56 +07:00",
-            "description": "Ipsum exercitation Lorem aliquip non veniam ipsum exercitation velit aute. Ex velit excepteur ut reprehenderit dolor sunt excepteur proident. Ut mollit sunt est laborum deserunt tempor reprehenderit ex eu est et officia proident nulla. Velit deserunt qui magna nulla reprehenderit tempor esse aliquip consectetur. Veniam velit id tempor minim in aliquip. Irure officia magna enim nulla tempor ea laboris nulla Lorem cillum. Elit consequat proident consectetur fugiat sunt sunt.\r\n"
-        }, {
-            "id": 886628,
-            "productName": "Nexgene",
-            "quanity": 38,
-            "price": 84210,
-            "auctionEnds": "2015-10-07T07:55:33 +07:00",
-            "priceReduces": "2015-10-20T09:06:31 +07:00",
-            "description": "Adipisicing dolore ea deserunt est duis. Ullamco Lorem sit ea reprehenderit dolore esse do ad. Proident elit commodo incididunt minim in amet laboris. Minim dolore tempor fugiat duis occaecat commodo culpa eiusmod sit reprehenderit incididunt duis proident sit. Nulla consequat ad et magna enim ad id et.\r\n"
-        }, {
-            "id": 1448587,
-            "productName": "Decratex",
-            "quanity": 8,
-            "price": 46650,
-            "auctionEnds": "2015-08-14T11:46:36 +07:00",
-            "priceReduces": "2015-10-27T11:19:26 +07:00",
-            "description": "Minim sit velit incididunt velit proident dolore labore. Culpa officia proident pariatur nulla duis eiusmod ullamco. Duis esse irure laboris amet nulla amet voluptate cupidatat ad Lorem. In officia veniam aliquip officia elit esse cillum id est cupidatat irure sit enim.\r\n"
-        }, {
-            "id": 1824301,
-            "productName": "Zillidium",
-            "quanity": 196,
-            "price": 15430,
-            "auctionEnds": "2015-04-14T04:46:42 +07:00",
-            "priceReduces": "2015-06-06T11:22:47 +07:00",
-            "description": "Non occaecat incididunt exercitation culpa culpa laboris Lorem irure deserunt nulla id. Cillum pariatur cillum quis sit dolore voluptate adipisicing ad do elit duis eu enim. Ipsum excepteur nulla fugiat sit ex in proident sunt enim mollit aliquip magna deserunt. Tempor cillum ea nulla ad esse culpa est quis ex.\r\n"
-        }, {
-            "id": 1041954,
-            "productName": "Geekol",
-            "quanity": 26,
-            "price": 29830,
-            "auctionEnds": "2015-04-25T03:22:46 +07:00",
-            "priceReduces": "2015-11-07T08:11:52 +08:00",
-            "description": "Amet ullamco nostrud veniam eu elit minim sint culpa commodo sunt ullamco. Dolor amet est officia eiusmod aliquip adipisicing. Proident laboris aliqua ullamco in. Aute esse ut veniam proident sint aute et sit quis cupidatat. Voluptate anim eiusmod officia ea commodo eu non Lorem consequat. Quis labore anim dolor minim eu exercitation irure dolor.\r\n"
-        }, {
-            "id": 23156,
-            "productName": "Utarian",
-            "quanity": 122,
-            "price": 70590,
-            "auctionEnds": "2015-04-26T06:03:45 +07:00",
-            "priceReduces": "2015-06-04T03:14:21 +07:00",
-            "description": "Nulla quis est ea voluptate dolore labore dolor labore anim. Amet veniam ut consequat anim. Laborum in officia fugiat voluptate aliquip aliqua labore minim ex ea fugiat excepteur cillum. Minim ad officia voluptate culpa fugiat incididunt voluptate ut aute ut cupidatat ad consequat. Aliquip qui amet officia sint proident occaecat est fugiat dolor eu commodo. Proident ipsum esse officia consequat do velit eu voluptate pariatur exercitation. Commodo consequat do minim cupidatat sit dolor nulla cupidatat cupidatat eiusmod labore qui exercitation.\r\n"
-        }, {
-            "id": 862246,
-            "productName": "Olucore",
-            "quanity": 51,
-            "price": 84480,
-            "auctionEnds": "2015-04-26T10:46:16 +07:00",
-            "priceReduces": "2015-07-09T01:45:37 +07:00",
-            "description": "Est et laboris et officia officia enim et dolor proident reprehenderit aliquip laborum enim. Id occaecat incididunt nulla exercitation in consequat. Labore laborum ullamco velit dolore consectetur aliquip aliqua sint occaecat fugiat. Ipsum sint qui dolore eu ullamco ut. Est mollit enim laborum occaecat Lorem et fugiat irure nostrud do ea pariatur aliqua. Ex quis voluptate aliquip elit id laborum nisi duis culpa elit aliqua.\r\n"
-        }, {
-            "id": 526697,
-            "productName": "Zolar",
-            "quanity": 193,
-            "price": 85110,
-            "auctionEnds": "2015-12-22T05:31:07 +08:00",
-            "priceReduces": "2015-07-13T09:48:32 +07:00",
-            "description": "Enim occaecat sunt ea labore velit incididunt nisi proident pariatur id. Eu cillum proident ut commodo pariatur minim nisi culpa amet magna reprehenderit non veniam. Ex laborum voluptate eu occaecat qui aliqua incididunt. Veniam laboris irure occaecat minim irure anim dolore. Minim ex minim ex voluptate anim ut quis quis.\r\n"
-        }]
-    }
+    "items": [{
+        "id": 1051645,
+        "productName": "Zillanet",
+        "quanity": 107,
+        "price": 88560,
+        "auctionEnds": "2015-11-20T09:31:29 +08:00",
+        "priceReduces": "2015-04-30T09:30:31 +07:00",
+        "description": "Enim est ipsum consequat deserunt proident est. Fugiat magna eiusmod fugiat ullamco labore qui culpa velit irure laboris laborum quis id officia. Sunt velit occaecat ea elit ipsum exercitation excepteur enim qui. Incididunt incididunt dolor in aute enim incididunt consectetur irure ipsum.\r\n"
+    }, {
+        "id": 1239391,
+        "productName": "Papricut",
+        "quanity": 32,
+        "price": 53540,
+        "auctionEnds": "2015-05-31T08:21:19 +07:00",
+        "priceReduces": "2016-01-12T07:54:01 +08:00",
+        "description": "In ad sit amet et. Commodo excepteur proident culpa elit irure fugiat proident ut consequat ut pariatur fugiat. Laborum reprehenderit anim sit qui elit est occaecat. Deserunt cillum tempor dolor eu. Excepteur exercitation qui dolor laboris in eu minim ullamco fugiat. Id aliqua non in nulla sit deserunt ipsum sit nisi aliquip sunt exercitation.\r\n"
+    }, {
+        "id": 38197,
+        "productName": "Buzzmaker",
+        "quanity": 59,
+        "price": 64990,
+        "auctionEnds": "2015-09-14T05:26:05 +07:00",
+        "priceReduces": "2015-06-24T08:32:56 +07:00",
+        "description": "Ipsum exercitation Lorem aliquip non veniam ipsum exercitation velit aute. Ex velit excepteur ut reprehenderit dolor sunt excepteur proident. Ut mollit sunt est laborum deserunt tempor reprehenderit ex eu est et officia proident nulla. Velit deserunt qui magna nulla reprehenderit tempor esse aliquip consectetur. Veniam velit id tempor minim in aliquip. Irure officia magna enim nulla tempor ea laboris nulla Lorem cillum. Elit consequat proident consectetur fugiat sunt sunt.\r\n"
+    }, {
+        "id": 886628,
+        "productName": "Nexgene",
+        "quanity": 38,
+        "price": 84210,
+        "auctionEnds": "2015-10-07T07:55:33 +07:00",
+        "priceReduces": "2015-10-20T09:06:31 +07:00",
+        "description": "Adipisicing dolore ea deserunt est duis. Ullamco Lorem sit ea reprehenderit dolore esse do ad. Proident elit commodo incididunt minim in amet laboris. Minim dolore tempor fugiat duis occaecat commodo culpa eiusmod sit reprehenderit incididunt duis proident sit. Nulla consequat ad et magna enim ad id et.\r\n"
+    }, {
+        "id": 1448587,
+        "productName": "Decratex",
+        "quanity": 8,
+        "price": 46650,
+        "auctionEnds": "2015-08-14T11:46:36 +07:00",
+        "priceReduces": "2015-10-27T11:19:26 +07:00",
+        "description": "Minim sit velit incididunt velit proident dolore labore. Culpa officia proident pariatur nulla duis eiusmod ullamco. Duis esse irure laboris amet nulla amet voluptate cupidatat ad Lorem. In officia veniam aliquip officia elit esse cillum id est cupidatat irure sit enim.\r\n"
+    }, {
+        "id": 1824301,
+        "productName": "Zillidium",
+        "quanity": 196,
+        "price": 15430,
+        "auctionEnds": "2015-04-14T04:46:42 +07:00",
+        "priceReduces": "2015-06-06T11:22:47 +07:00",
+        "description": "Non occaecat incididunt exercitation culpa culpa laboris Lorem irure deserunt nulla id. Cillum pariatur cillum quis sit dolore voluptate adipisicing ad do elit duis eu enim. Ipsum excepteur nulla fugiat sit ex in proident sunt enim mollit aliquip magna deserunt. Tempor cillum ea nulla ad esse culpa est quis ex.\r\n"
+    }, {
+        "id": 1041954,
+        "productName": "Geekol",
+        "quanity": 26,
+        "price": 29830,
+        "auctionEnds": "2015-04-25T03:22:46 +07:00",
+        "priceReduces": "2015-11-07T08:11:52 +08:00",
+        "description": "Amet ullamco nostrud veniam eu elit minim sint culpa commodo sunt ullamco. Dolor amet est officia eiusmod aliquip adipisicing. Proident laboris aliqua ullamco in. Aute esse ut veniam proident sint aute et sit quis cupidatat. Voluptate anim eiusmod officia ea commodo eu non Lorem consequat. Quis labore anim dolor minim eu exercitation irure dolor.\r\n"
+    }, {
+        "id": 23156,
+        "productName": "Utarian",
+        "quanity": 122,
+        "price": 70590,
+        "auctionEnds": "2015-04-26T06:03:45 +07:00",
+        "priceReduces": "2015-06-04T03:14:21 +07:00",
+        "description": "Nulla quis est ea voluptate dolore labore dolor labore anim. Amet veniam ut consequat anim. Laborum in officia fugiat voluptate aliquip aliqua labore minim ex ea fugiat excepteur cillum. Minim ad officia voluptate culpa fugiat incididunt voluptate ut aute ut cupidatat ad consequat. Aliquip qui amet officia sint proident occaecat est fugiat dolor eu commodo. Proident ipsum esse officia consequat do velit eu voluptate pariatur exercitation. Commodo consequat do minim cupidatat sit dolor nulla cupidatat cupidatat eiusmod labore qui exercitation.\r\n"
+    }, {
+        "id": 862246,
+        "productName": "Olucore",
+        "quanity": 51,
+        "price": 84480,
+        "auctionEnds": "2015-04-26T10:46:16 +07:00",
+        "priceReduces": "2015-07-09T01:45:37 +07:00",
+        "description": "Est et laboris et officia officia enim et dolor proident reprehenderit aliquip laborum enim. Id occaecat incididunt nulla exercitation in consequat. Labore laborum ullamco velit dolore consectetur aliquip aliqua sint occaecat fugiat. Ipsum sint qui dolore eu ullamco ut. Est mollit enim laborum occaecat Lorem et fugiat irure nostrud do ea pariatur aliqua. Ex quis voluptate aliquip elit id laborum nisi duis culpa elit aliqua.\r\n"
+    }, {
+        "id": 526697,
+        "productName": "Zolar",
+        "quanity": 193,
+        "price": 85110,
+        "auctionEnds": "2015-12-22T05:31:07 +08:00",
+        "priceReduces": "2015-07-13T09:48:32 +07:00",
+        "description": "Enim occaecat sunt ea labore velit incididunt nisi proident pariatur id. Eu cillum proident ut commodo pariatur minim nisi culpa amet magna reprehenderit non veniam. Ex laborum voluptate eu occaecat qui aliqua incididunt. Veniam laboris irure occaecat minim irure anim dolore. Minim ex minim ex voluptate anim ut quis quis.\r\n"
+    }]
 };
