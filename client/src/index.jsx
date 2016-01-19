328da800fe4a4feaa24b02ab9fa7f93ce3581c72
seanagibson <seanandgibson@gmail.com>
2016-01-18 19:52:47 -0800

    [refactor]: refactors index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router, Route, browserHistory} from 'react-router';
-import {syncHistory, routeReducer} from 'redux-simple-router';
+import {createHistory, useBasename} from 'history';
+import {syncReduxAndRouter} from 'redux-simple-router';
 import App from './containers/app';
-import Browse from './containers/browse';
-import Dashboard from './containers/dashboard';
 
-const reduxRouterMiddleware = syncHistory(browserHistory);
+const history = useBasename(createHistory)({
+  basename: '/'
+});
+
+const store = configureStore(window.__INITIAL_STATE__);
+
+syncReduxAndRouter(history, store, (state) => state.router);
 
 const app = document.createElement('div');
 document.body.appendChild(app);
-
-ReactDOM.render(
-  <Provider>
-    <Router history={browserHistory}>
-      <Route path='/' component={App}>
-        <Route path="browse" component={Browse}/>
-        <Route path="dashboard" component={Dashboard}/>
-      </Route>
-    </Router>
-  </Provider>, app);
+ReactDOM.render(<App store={store} history={history} routes={routes} />, app);
