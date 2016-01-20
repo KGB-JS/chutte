c810fc00d683a8c93a4d0ef8ab34c961946bd5d3
seanagibson <seanandgibson@gmail.com>
2016-01-19 16:49:50 -0800

    [fix]: fixes issue with newState variable

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -12,19 +12,18 @@ const initialState = Map({
 });
 
 function products(state = initialState, action){
+  let newState = state;
+
   switch(action.type){
     case GET_PRODUCTS:
-      let newState = state;
       newState.set('isFetchingProducts', true);
       return newState;
     case GET_PRODUCTS_SUCCESS:
-      let newState = state;
       newState.set('isFetchingProducts', false);
       newState.set('fetchedProducts', true);
       newState.set('products', List(action.products));
       return newState;
     case GET_PRODUCTS_FAILURE:
-      let newState = state;
       newState.set('isFetchingProducts', false);
       newState.set('fetchedProducts', false);
       newState.set('fetchStatus', action.err);
@@ -35,19 +34,18 @@ function products(state = initialState, action){
 };
 
 function createListing(state = initialState, action){
+  let newState = state;
+
   switch(action.type){
     case CREATE_LISTING:
-      let newState = state;
       newState.set('isPostingProduct', true);
       return newState;
     case CREATE_LISTING_SUCCESS:
-      let newState = state;
       newState.set('isPostingProduct', false);
       newState.set('postedProduct', true);
       newState.updateIn('products', newState.get('products').push(action.productedListed));
       return newState;
     case CREATE_LISTING_FAILURE:
-      let newState = state;
       newState.set('isFetchingProducts', false);
       newState.set('postStatus', action.err);
       return newState;
