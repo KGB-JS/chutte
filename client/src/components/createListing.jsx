4ea9ca369eaf977fdef7cf5875f36fa1a4c14bea
steele2345 <beresford211@users.noreply.github.com>
2016-01-21 11:51:47 -0800

    [feat]: add ability to create item listing

---
+++ client/src/components/createListing.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {createListing} from './../actions/actionsCreateListing';
+import CategorySelection from './CategorySelection';
+
+class CreateListing extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  submitForm(){
+    var itemDetails {
+      productName: this.refs.name.value
+      createdBy : this.refs.name.value
+      category : this.refs.category.value
+      quantity : this.refs.quantity.value
+      price: this.refs.price.value
+      minPrice: this.refs.minPrice.value
+    };
+
+    this.props.submitListing(itemDetails);
+  }
+
+
+
+  render() {
+    return (
+      <form role="form">
+      <div>
+        <label>Name</label>
+        <input type="text" ref="name" />
+
+        <label>Quantity</label>
+        <input type="text" ref="quantity"/>
+
+        <label>Price</label>
+        <input type="text" ref="price" />
+        <label>Minimum Sales Price</label>
+        <input type="text" ref="minPrice" />
+     
+        <button type="button" onClick={this.submitForm.bind(this)}>List Item</button>
+      </div>
+      </form>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    submitListing: function(itemDetails){
+      dispatch(createListing(itemDetails))
+    }
+  }
+}
+
+
+
+export default connect(mapDispatchToProps)(CreateListing);
