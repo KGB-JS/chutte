5aa8c56fde0861de2f38ba01ff34d042dea56e71
seanagibson <seanandgibson@gmail.com>
2016-01-21 15:29:19 -0800

    [refactor]: adds product img url

--- client/src/components/productImage.jsx
+++ client/src/components/productImage.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 class ProductImage extends React.Component {
   render(){
+console.log('-------Product Image props: ', this.props);
     return (
-      <div><img src={this.props.image.imageURL} alt="ProductImage" className="product-image"/></div>
+      <div><img src={this.props.image} alt="ProductImage" className="product-image"/></div>
     )
   }
 }
