a8eeab378a10033a26e21dd42a68171f9047f1d7
seanagibson <seanandgibson@gmail.com>
2016-01-22 12:11:58 -0800

    [refactor]: changes import in socket.js

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,9 +1,9 @@
-import actions from './../actions/actionsProducts';
+import {updateProduct} from './../actions/actionsProducts';
 var socket = require('socket.io-client')();
 
 export default function(store){
 
   socket.on('productUpdate', function(obj){
-    store.dispatch(actions.updateProduct(obj));
+    store.dispatch(updateProduct(obj));
   });
 }
