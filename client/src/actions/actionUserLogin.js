0aa02171463d298f78b885c47b97ecddf029e7db
seanagibson <seanandgibson@gmail.com>
2016-01-21 17:19:12 -0800

    [feat]: adds actions for user login

---
+++ client/src/actions/actionUserLogin.js
@@ -0,0 +1,44 @@
+import {fetch} from 'isomorphic-fetch';
+import {USER_LOGIN, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE} from './actionConstants';
+
+export function userLogin(userName){
+  return {
+    type: USER_LOGIN,
+    userName: userName
+  };
+};
+
+export function userLoginSuccess(token){
+  localStorage.setItem('token', token);
+  return {
+    type: USER_LOGIN_SUCCESS,
+    token: token
+  };
+};
+
+export function userLoginFailure(err){
+  return {
+    type: USER_LOGIN_FAILURE,
+    err: err
+  };
+};
+
+export function authenticateUser(user){
+  return function(dispatch){
+    dispatch(userLogin);
+    return fetch('/api/users/signin', , {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(function(response){
+      dispatch(userLoginSuccess(response.token));
+    })
+    .catch(function(error){
+      dispatch(userLoginFailure(error));
+    });
+  };
+};
