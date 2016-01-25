7bd169c137ae10b7e183fe6e126379f93f921421
steele2345 <beresford211@users.noreply.github.com>
2016-01-25 15:11:14 -0800

    [feat]: added sign up page for users

--- client/src/components/browseSidebar.jsx
+++ client/src/components/browseSidebar.jsx
@@ -4,16 +4,16 @@ import {Link} from 'react-router';
 export default class BrowseSideNavbar extends React.Component {
 
   render() {
-    var category = ["Antiques", "Art", "Automobiles", "clothing", "electronics",
+    var category = ["Antiques", "Art", "Automobiles", "Clothing", "Electronics",
     "Entertainment", "Jewelry", "Tickets and Experiences", "Travel"];
     return (
-         <div className="col-sm-3 col-md-2 sidebar">
-          <ul className="nav nav-sidebar browse">
-              {category.map(function(subCategory, index) {
-                return <li key={index}><a href={index}>{subCategory}</a></li>;
-              })}
-          </ul>
-        </div>
+      <div className="col-sm-3 col-md-2 sidebar">
+        <ul className="nav nav-sidebar browse">
+            {category.map(function(subCategory, index) {
+              return <li key={index}><a href={index}>{subCategory}</a></li>;
+            })}
+        </ul>
+      </div>
     );
   }
 };
---
+++ client/src/components/userSignup.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {createListing, postListing} from './../actions/actionsCreateListing';
+
+
+export default class UserSignup extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {};
+  }
+
+  submitSignUp(){
+    var signupDetails = {
+      firstname : String(this.state.categorySelected),
+      lastname : String(this.refs.quantity.value),
+      phone : Number(this.resf.phone.value),
+      address: String(this.refs.address.value),
+      country: String(this.refs.phone.value),
+      zip: Number(this.refs.zip.value),
+      state: String(this.refs.state.value),
+      city: String(this.refs.city.value),
+      username: String(this.refs.username.value),
+      password : String(this.refs.name.value)
+    };
+    console.log(signupDetails);
+    this.props.submitListing(itemDetails);
+  }
+
+  render() {
+    var userAttributes = ["firstname", "lastname", "phone", "address", "country",
+    "zip", "state", "city", "username", "password"];
+            
+    return (
+      <div className="form col-md-3 userinput">
+       {userAttributes.map(function(attribute, index) {
+          return ( 
+              <div>
+                <label>{attribute}</label>
+                <input key={index} ref={attribute}/>
+              </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default UserSignup;
