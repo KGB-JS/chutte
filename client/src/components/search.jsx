15211293d154f3f0128ec94971124ac4bec2e222
steele2345 <beresford211@users.noreply.github.com>
2016-01-25 11:33:02 -0800

    [style] styled pages with css

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -8,10 +8,11 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
-        <input 
+      <div className="search-bar">
+        <input className="search-bar"
           value = {this.state.searchVal}
           onChange={(event) => this.onChangeFunc(event.target.value)} />
+          <button className="butn btn-default" type="button">Search</button>
       </div>
     )
   }
