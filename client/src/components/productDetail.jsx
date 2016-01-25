a2a17f94a6c38b32130d848db5c598b3ad47f886
seanagibson <seanandgibson@gmail.com>
2016-01-25 14:54:58 -0800

    [refactor]: adds redux state to display product details

--- client/src/components/productDetail.jsx
+++ client/src/components/productDetail.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import ProductImage from './productImage';
 import BuyButton from './buyButton';
 
 class ProductDetail extends React.Component {
+  constructor (props) {
+    super(props)
+    this.productId = this.props.params.id;
+  }
+
   render() {
     return (
-      <div className="productCard" id={this.props.productDetail._id}>
-        <ProductImage image={this.props.productDetail.imageURL}/>
+      <div className="productCard" id={this.props.products[this.productId]._id}>
+        <ProductImage image={this.props.products[this.productId].imageURL}/>
         <div className="productName">
-          <p>Product: {this.props.productDetail.productName}</p>
+          <p>Product: {this.props.products[this.productId].productName}</p>
         </div>
 
         <div className="productTime">
-          <p>Time Remaining: {this.props.productDetail.priceReduces}</p>
+          <p>Time Remaining: {this.props.products[this.productId].priceReduces}</p>
         </div>
 
         <div className="productQuantity">
-          <p>Quantity: {this.props.productDetail.quantity}</p>
+          <p>Quantity: {this.props.products[this.productId].quantity}</p>
         </div>
 
         <div className="productPrice">
-          <p>Price: ${this.props.productDetail.price}</p>
+          <p>Price: ${this.props.products[this.productId].price}</p>
         </div>
       </div>
     );
   }
-};
+}
 
-export default ProductDetail;
+function mapStateToProps(state){
+  return {products: state.productStore.products.productList};
+}
+
+export default connect(mapStateToProps)(ProductDetail)
