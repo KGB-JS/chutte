9aaf9584d2ac24797fc3292bee98fa882bcc8a23
seanagibson <seanandgibson@gmail.com>
2016-01-24 19:59:20 -0800

    [refactor]: refactors app.jsx and index.jsx

--- client/src/containers/app.jsx
+++ client/src/containers/app.jsx
@@ -1,26 +1,14 @@
 import React, {PropTypes} from 'react';
-import {Provider} from 'react-redux';
-import {Router} from 'react-router';
 import DevTools from './devTools';
 
 
 export default class App extends React.Component {
-  static propTypes = {
-    store: PropTypes.object.isRequired,
-    routes: PropTypes.element.isRequired,
-    history: PropTypes.object.isRequired
-  };
-
   render() {
     return (
-      <Provider store={this.props.store}>
-        <div>
-          <Router history={this.props.history}>
-            {this.props.routes}
-          </Router>
-          <DevTools/>
-        </div>
-      </Provider>
+      <div>
+        {this.props.children}
+        <DevTools/>
+       </div>
     )
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,21 +3,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createHistory, useBasename} from 'history';
+import {Router, Route, IndexRoute, Redirect} from 'react-router';
 import {syncReduxAndRouter} from 'redux-simple-router';
 import configureStore from './store/configStore';
 import socket from './socket/socket';
-import routes from './routes';
 import App from './containers/app';
+import Home from './containers/home';
+import Browse from './containers/browse';
+import Dashboard from './containers/dashboard';
 
 const history = useBasename(createHistory)({
   basename: '/'
 });
 
-const store = configureStore(window.__INITIAL_STATE__);
-socket(store);
+const store = configureStore();
 
 syncReduxAndRouter(history, store, (state) => state.router);
 
+socket(store);
+
 const app = document.createElement('div');
 document.body.appendChild(app);
-ReactDOM.render(<App store={store} history={history} routes={routes} />, app);
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+       <Route path='/' component={App}>
+        <IndexRoute component={Home}/>
+        <Route path='browse' component={Browse}/>
+        <Route path='dashboard' component={Dashboard}/>
+        <Redirect from='*' to='/' />
+      </Route>
+    </Router>
+  </Provider>, app);
