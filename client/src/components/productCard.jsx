881b1745dfa2ccc3c041a98eebb534177b8bccac
seanagibson <seanandgibson@gmail.com>
2016-01-25 14:58:29 -0800

    [refactor]: adds link to product detail

--- client/src/components/productCard.jsx
+++ client/src/components/productCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 import ProductImage from './productImage';
 import BuyButton from './buyButton';
 
@@ -7,11 +8,11 @@ class ProductCard extends React.Component {
     return (
       <div className="productCard col-sm-6 col-md-4" id={this.props.productDetail._id}>
         <div className="productName">
-          <p> {this.props.productDetail.productName} </p>
+          <Link to={`browse/${this.props.productDetail._id}`}> {this.props.productDetail.productName} </Link>
         </div>
 
         <ProductImage className="productImage" image={this.props.productDetail.imageURL}/>
-        
+
         <row>
           <div className="productTime col-md-4">
             <p className="productinfo">Time Left: {this.props.productDetail.priceReduces}</p>
