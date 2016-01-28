c102f45fa634a1e16c8f0283a859c960adb8fdf5
peterkaiserui <peterkaiserui@gmail.com>
2016-01-27 19:22:36 -0800

    [fix]: nav links fix

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,17 +4,18 @@ import {Link} from 'react-router';
 export default class Navbar extends React.Component {
   render() {
     var icon = (
-    <span>
-      <a href="/" >
-        <img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" /> </a>
-    </span>
+    
+      
+        <img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />
+
   )
  
     return (
       <nav className="navbar navbar-default">
         <div className="container-fluid chutteGreen">
           <div className="navbar-header">
-            <Link className="navbar-brand" to="/">{icon} Chutte</Link>
+          {icon}
+            <Link className="navbar-brand" to="/"> Chutte</Link>
           </div>
           <ul className="nav navbar-nav navbar-right">
               <li><Link to="/browse"><span className="glyphicon glyphicon-shopping-cart"></span> Browse</Link></li>
