6532421d12a974d5e6bdcd12bc161b6b7633fa48
steele2345 <beresford211@users.noreply.github.com>
2016-01-28 15:52:15 -0800

    [feat]: added dashboardTiles

---
+++ client/src/components/dashboardTiles.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class DashBoardTile extends React.Component {
+  render(){
+    return ( 
+      <div>
+        <div className="dashboardBox">
+          <h1></h1>
+          <p>This is a test of the dashboard page title. This is a test of the dashboard page titld page titleThis is a test of the dashboard page title </p>
+        </div>
+        <div className="dashboardBox">        
+          <h1></h1>
+          <p>This is a test of the dashboard page title. This is a test of the dashboard page titld page titleThis is a test of the dashboard page title </p>
+        </div>
+        <div className="dashboardBox">        
+          <h1></h1>
+          <p>This is a test of the dashboard page title. This is a test of the dashboard page titld page titleThis is a test of the dashboard page title </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DashBoardTile;
