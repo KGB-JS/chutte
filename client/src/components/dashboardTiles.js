7e94dd78b04356097979441209d8b8e8763fae8a
peterkaiserui <peterkaiserui@gmail.com>
2016-01-28 18:50:18 -0800

    [style]: add information to home page and css

--- client/src/components/dashboardTiles.js
+++ client/src/components/dashboardTiles.js
@@ -3,18 +3,38 @@ import React from 'react';
 class DashBoardTile extends React.Component {
   render(){
     return ( 
-      <div>
-        <div className="dashboardBox">
-          <h1></h1>
-          <p>This is a test of the dashboard page title. This is a test of the dashboard page titld page titleThis is a test of the dashboard page title </p>
+      <div className="container-fluid">
+        <div id="infoHeader">
+        Information
         </div>
-        <div className="dashboardBox">        
-          <h1></h1>
-          <p>This is a test of the dashboard page title. This is a test of the dashboard page titld page titleThis is a test of the dashboard page title </p>
+        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div className="panel panel-primary">
+          <div className="panel-heading"><span className="glyphicon glyphicon-time"></span>
+          </div>
+            <div className="panel-body">
+            <p>Ut scelerisque nisi sit amet erat pretium, id dignissim orci fringilla. Integer nec pellentesque libero, in facilisis eros. Vestibulum interdum efficitur lacus id posuere. Quisque tincidunt aliquet felis posuere porttitor. Praesent eget arcu condimentum, finibus ex et, tempor arcu. Morbi dictum, dui non maximus tempor, arcu quam feugiat dui, sit amet lacinia justo nisi nec nisl. Curabitur velit lorem, rutrum a tellus vel, maximus lobortis urna. Vestibulum elementum libero ipsum, sit amet varius urna placerat ac.</p>
+            </div>
+          </div>
         </div>
-        <div className="dashboardBox">        
-          <h1></h1>
-          <p>This is a test of the dashboard page title. This is a test of the dashboard page titld page titleThis is a test of the dashboard page title </p>
+        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div className="panel panel-primary">
+          <div className="panel-heading">
+          <span className="glyphicon glyphicon-usd"></span>
+          </div>
+            <div className="panel-body">
+            <p>Ut scelerisque nisi sit amet erat pretium, id dignissim orci fringilla. Integer nec pellentesque libero, in facilisis eros. Vestibulum interdum efficitur lacus id posuere. Quisque tincidunt aliquet felis posuere porttitor. Praesent eget arcu condimentum, finibus ex et, tempor arcu. Morbi dictum, dui non maximus tempor, arcu quam feugiat dui, sit amet lacinia justo nisi nec nisl. Curabitur velit lorem, rutrum a tellus vel, maximus lobortis urna. Vestibulum elementum libero ipsum, sit amet varius urna placerat ac.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div className="panel panel-primary">
+          <div className="panel-heading">
+          <span className="glyphicon glyphicon-tags"></span>
+          </div>
+            <div className="panel-body">
+            <p>Ut scelerisque nisi sit amet erat pretium, id dignissim orci fringilla. Integer nec pellentesque libero, in facilisis eros. Vestibulum interdum efficitur lacus id posuere. Quisque tincidunt aliquet felis posuere porttitor. Praesent eget arcu condimentum, finibus ex et, tempor arcu. Morbi dictum, dui non maximus tempor, arcu quam feugiat dui, sit amet lacinia justo nisi nec nisl. Curabitur velit lorem, rutrum a tellus vel, maximus lobortis urna. Vestibulum elementum libero ipsum, sit amet varius urna placerat ac.</p>
+            </div>
+          </div>
         </div>
       </div>
     )
--- client/src/containers/home.jsx
+++ client/src/containers/home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import HomeNavBar from './../components/homenavbar';
 import JumboTronButton from './../components/JumboTronButton';
+import DashboardTiles from './../components/dashboardTiles';
 
 export default class Home extends React.Component {
   render() {
     return (
+        <div>
       <div className="full">
         <HomeNavBar/>
         <JumboTronButton/>
+      </div>
+      <div className="homeInfo">
+      <DashboardTiles/>
+      </div>
       </div>
     )
   }
