cb339b49a24d3ddb41cb20793d80c17de323d345
Michael Berber <mickberber@icloud.com>
2016-01-28 17:00:23 -0800

    [feat]: img on product card

--- client/src/components/productImage.jsx
+++ client/src/components/productImage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 class ProductImage extends React.Component {
   render(){
+  	console.log(this.props.image)
     return (
       <div className="productImage"><img className="displayed" src={this.props.image}  /></div>
     )
