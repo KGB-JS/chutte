8b5746b9e5aef2019a2bb42b2aa5417b147c03b9
seanagibson <seanandgibson@gmail.com>
2016-01-29 15:09:41 -0800

    [refactor]: replaces <a> tag with Link tag

--- client/src/components/homenavbar.jsx
+++ client/src/components/homenavbar.jsx
@@ -5,16 +5,14 @@ export default class HomeNavbar extends React.Component {
   render() {
     var icon = (
     <span>
-      <a href="/" >
-        <img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" /> </a>
+        <img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />
     </span>
   )
     return (
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a  href="#" className="navbar-brand">{icon}</a>
-               Chutte
+            <Link  to="/" className="navbar-brand">{icon} Chutte</Link>
           </div>
           <ul className="nav navbar-nav navbar-right">
               <li><Link to='signup'>Sign Up</Link></li>
