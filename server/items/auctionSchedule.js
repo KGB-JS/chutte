0772b7c6fff07dfe4495c5d7626d79b00f21447a
Michael Berber <mickberber@icloud.com>
2016-02-01 11:28:47 -0800

    [feat]: update auction schedule to 15 min intervals

---
+++ server/items/auctionSchedule.js
@@ -0,0 +1,33 @@
+var moment = require('moment');
+
+module.exports = {
+    
+    findTimeReduce: function(currentPrice, minPrice, endDate) {
+        var now = moment().valueOf();
+        var totalAuctionTime = endDate - now;
+        var numberOfDecrements = totalAuctionTime/900000;
+        var amountToDecrementPrice = (currentPrice - minPrice)/numberOfDecrements;
+        var results = [];
+        var priceSchedule = [];
+        var decrementTime = now;
+        while(decrementTime < endDate && currentPrice >= minPrice){
+            decrementTime = decrementTime + 900000;
+            currentPrice = Math.floor(currentPrice - amountToDecrementPrice);
+            if(currentPrice >= minPrice && decrementTime < endDate){
+                var priceIndex = {
+                    price: currentPrice,
+                    decrementTime: decrementTime
+                };
+                priceSchedule.push(priceIndex);
+                console.log('pI', priceIndex);
+            }
+        }
+        results.push(priceSchedule);
+        results.push(900000);
+        console.log('results',results);
+        console.log('endDate', endDate);
+        return results
+    }
+
+        
+}
