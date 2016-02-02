bfe617bc55f8591841a36b5927c7c024fd289f52
Michael Berber <mickberber@icloud.com>
2016-02-01 16:19:02 -0800

    [feat]: browse and dashboard sidebar style

--- client/src/components/browseSidebar.jsx
+++ client/src/components/browseSidebar.jsx
@@ -13,10 +13,7 @@ export default class BrowseSideNavbar extends React.Component {
   }
   render() {
     let categories = CategoryFilters;
-        var icon = (
-    
-      
-        <img className="logo" src="/assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />)
+        var icon = (<img className="logo" src="/assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />)
     return (
         <div id="wrapper">
           <div id="sidebar-wrapper">
--- client/src/components/profileSideBar.jsx
+++ client/src/components/profileSideBar.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {UserOptions} from './../actions/actionConstants';
 
 export default class ProfileSideNavbar extends React.Component {
-
+    constructor(props){
+       super(props)
+    }
+    handleFilter(e){
+       e.preventDefault();
+       this.props.filter(e.target.id);
+    }
   render() {
     var userOptions = ["User Profile", "Create Listing", "Current Listing"];
-
+    var icon = (<img className="logo" src="/assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />)
     return (
-      <div className="col-sm-3 col-md-2 sideBar profileBar">
-        <ul className="nav nav-sidebar browse">
-          { userOptions.map(function(option, index){
-            return <li key={index}><a href="#">{option}</a></li>     
-          }) }
-        </ul>
-      </div>
+        <div id="wrapper">
+          <div id="sidebar-wrapper">
+          <Link className="navbar-brand" to="/"> {icon} Chutte</Link>
+            <ul className="sidebar-font">
+              <li id={userOptions[0]} onClick={this.handleFilter.bind(this)}>{userOptions[0]}</li>
+              <li id={userOptions[1]} onClick={this.handleFilter.bind(this)}>{userOptions[1]}</li>
+              <li id={userOptions[2]} onClick={this.handleFilter.bind(this)}>{userOptions[2]}</li>
+            </ul>
+          </div>
+        </div>
     );
   }
 };
