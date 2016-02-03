08ada34f1def15f5dbdf0d6053759f3cf4d35105
seanagibson <seanandgibson@gmail.com>
2016-02-02 23:14:50 -0800

    [refactor]: adds navbar and connects to redux state

--- client/src/components/currentListing.jsx
+++ client/src/components/currentListing.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import ProductCard from './../components/productCard';
 import ProductList from './../components/productList';
+import NavBar from '../components/navbar';
+import ProfileSideNavBar from './../components/profileSideBar';
+import {userLogout} from './../actions/actionsUserAuth';
 
 class CurrentListing extends React.Component {
   render(){
     return (
     <div>
+      <NavBar submitSignout={this.props.submitSignout}
+         user={this.props.userAuth} />
+      <ProfileSideNavBar/>
         <div className="container-fluid">
           <row>
             <div className="col-xs-offset-2 col-sm-offset-2 col-sm-10 col-md-10 col-md-offset-2">
@@ -19,8 +24,19 @@ class CurrentListing extends React.Component {
   }
 }
 
-function mapStateToProps(state){
-  return {products: state.productStore.products.productList};
+function mapDispatchToProps(dispatch){
+  return {
+    submitSignout: function(){
+       dispatch(userLogout());
+    }
+  }
 }
 
-//export default connect(mapStateToProps)(BrowsePage);
+function mapStateToProps(state){
+  return {
+    products: state.productStore.products.productList,
+    userAuth: state.userStore.userAuth
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CurrentListing);
