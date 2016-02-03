075a4f179480328df8a2b225243caef05825b29c
seanagibson <seanandgibson@gmail.com>
2016-02-03 15:53:29 -0800

    [feat]: adds action creator to remove ended auctions

--- client/src/actions/actionsProducts.js
+++ client/src/actions/actionsProducts.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch';
 import {checkStatus, parseJSON} from './actionsHelper';
-import {GET_PRODUCTS, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_FAILURE, UPDATE_PRODUCT, POST_BUY, POST_BUY_SUCCESS, POST_BUY_FAILURE, PRODUCT_CATEGORY_FILTER, POST_BUY_RESET_MSG, REMOVE_SOLDOUT_PRODUCT} from './actionConstants';
+import {GET_PRODUCTS, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_FAILURE, UPDATE_PRODUCT, POST_BUY, POST_BUY_SUCCESS, POST_BUY_FAILURE, PRODUCT_CATEGORY_FILTER, POST_BUY_RESET_MSG, REMOVE_SOLDOUT_PRODUCT, REMOVE_ENDED_AUCTION_PRODUCT} from './actionConstants';
 
 export function updateProduct(product){
   return {
@@ -60,6 +60,12 @@ export function removeSoldoutProduct(product){
     type: REMOVE_SOLDOUT_PRODUCT,
     product: product
   }
+}
+
+export function removeEndedAuction(){
+  return {
+    type: REMOVE_ENDED_AUCTION_PRODUCT
+  };
 }
 
 export function filterByCategory(category){
