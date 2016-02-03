22be44d286109327f2aa05ef09eea6bfa9615b1b
peterkaiserui <peterkaiserui@gmail.com>
2016-02-03 10:35:34 -0800

    [style]: update profile css

--- client/src/components/profileSideBar.jsx
+++ client/src/components/profileSideBar.jsx
@@ -5,10 +5,12 @@ import {UserOptions} from './../actions/actionConstants';
 export default class ProfileSideNavbar extends React.Component {
   render() {
     var userOptions = ["User Profile", "Create Listing", "Current Listing"];
+    var icon = (<img className="logo" src="/assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" />)
 
     return (
         <div id="wrapper">
           <div id="sidebar-wrapper">
+           <Link className="navbar-brand" id="logo" to="/"> {icon} Chutte</Link>
             <ul className="sidebar-font">
               <li id={userOptions[0]}><Link to="dashboard/profile">{userOptions[0]}</Link></li>
               <li id={userOptions[1]}><Link to='/create'>{userOptions[1]}</Link></li>
