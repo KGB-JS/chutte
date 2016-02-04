fa030bae3d71a1983139f37cbd8d86f9d0abbe7b
seanagibson <seanandgibson@gmail.com>
2016-02-04 15:51:38 -0800

    [refactor]: adds signin/signup to browse navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
+import SignUpModal from './../components/signupModal';
+import SignInModal from './../components/loginModal';
 
 export default class Navbar extends React.Component {
   handleSignout(e){
@@ -8,23 +10,41 @@ export default class Navbar extends React.Component {
    }
 
   render() {
-    let signout = this.props.user.token !== '' ? <li onClick={this.handleSignout.bind(this)}><a>Sign out</a></li> : '';
     let icon = (<span><img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" /></span>);
-    return (
-      <nav className="navbar navbar-default navBrowse">
-        <div className="container-fluid chutteGreen">
-          <div className="navbar-header">
+
+    if(this.props.user.token !== ''){
+      return (
+        <nav className="navbar navbar-default navBrowse">
+          <div className="container-fluid chutteGreen">
             <div className="navbar-header">
-              <Link  to="/" className="navbar-brand">{icon} Chutte</Link>
+              <div className="navbar-header">
+                <Link  to="/" className="navbar-brand">{icon} Chutte</Link>
+              </div>
             </div>
-          </div>
-          <ul className="nav navbar-nav navbar-right">
+            <ul className="nav navbar-nav navbar-right">
               <li><Link to="/browse"><span className="glyphicon glyphicon-shopping-cart"></span> Browse</Link></li>
               <li><Link to="/dashboard/profile"><span className="glyphicon glyphicon-user"></span> Dashboard</Link></li>
-              {signout}
+              <li><a onClick={this.handleSignout.bind(this)}>Sign out</a></li>
             </ul>
-        </div>
-      </nav>
-    );
+          </div>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="navbar navbar-default navBrowse">
+          <div className="container-fluid chutteGreen">
+            <div className="navbar-header">
+              <div className="navbar-header">
+                <Link  to="/" className="navbar-brand">{icon} Chutte</Link>
+              </div>
+            </div>
+            <ul className="nav navbar-nav navbar-right">
+              <li><SignUpModal/></li>
+              <li><SignInModal/></li>
+            </ul>
+          </div>
+        </nav>
+      )
+    }
   }
 }
