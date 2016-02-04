2bb3c3ac2fd04c2d86b9dcd8a95408ef2d2b09ff
Michael Berber <mickberber@icloud.com>
2016-02-04 10:55:08 -0800

    [fix]: merge conflict

--- server/items/itemHelpers/emitAuction.js
+++ server/items/itemHelpers/emitAuction.js
@@ -17,29 +17,22 @@ module.exports = {
                     if (item.auctionEnds < now || item.quantity < 1) {
                         item.active = false;
                         item.save()
-                        clearTimeout(item.timeId[0]);
                     } else {
-                        var priceFlag = true;
-                        for (var i = 0; i < item.priceSchedule.length; i++) {
-                            if (priceFlag && item.priceSchedule[i].decrementTime > now) {
-                                priceFlag = false;
-                                item.price = item.priceSchedule[i].price;
-                                item.timeRemaining = item.auctionEnds;
-                                item.save();
-                                var transmitObject = {
-                                    _id: item._id,
-                                    price: item.price,
-                                    timeRemaining: item.auctionEnds,
-                                    description: item.description,
-                                    productName: item.productName,
-                                    createdBy: item.createdBy,
-                                    quantity: item.quantity,
-                                    category: item.category,
-                                    image: item.image
-                                };
-                                
-                            }
-                        }
+                        item.priceIndex++;
+                        item.save();
+                        var transmitObject = {
+                            _id: item._id,
+                            price: item.priceSchedule[item.priceIndex].price,
+                            timeRemaining: item.auctionEnds,
+                            description: item.description,
+                            productName: item.productName,
+                            createdBy: item.createdBy,
+                            quantity: item.quantity,
+                            category: item.category,
+                            image: item.image,
+                            auctionEnds: item.auctionEnds
+                        };
+                        console.log(transmitObject)
                         app.io.sockets.emit('productUpdate', transmitObject);
                     }
                 }
@@ -55,18 +48,21 @@ module.exports = {
                 if (!item) {
                     new Error('Item not found');
                 } else {
-                    var transmitObject = {
-                        _id: item._id,
-                        price: item.price,
-                        timeRemaining: item.auctionEnds,
-                        description: item.description,
-                        productName: item.productName,
-                        createdBy: item.createdBy,
-                        quantity: item.quantity,
-                        category: item.category,
-                        image: item.image
-                    };
-                    app.io.sockets.emit('productUpdate', transmitObject);
+                    if(item.priceIndex < item.priceSchedule.length){
+                        var transmitObject = {
+                            _id: item._id,
+                            price: item.priceSchedule[item.priceIndex].price,
+                            timeRemaining: item.auctionEnds,
+                            description: item.description,
+                            productName: item.productName,
+                            createdBy: item.createdBy,
+                            quantity: item.quantity,
+                            category: item.category,
+                            image: item.image,
+                            auctionEnds: item.auctionEnds
+                        };
+                        app.io.sockets.emit('productUpdate', transmitObject);
+                    }
                 }
             });
     }
