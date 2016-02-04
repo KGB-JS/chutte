7530a26805bd99b28ebd7b19218c4fab5307544e
peterkaiserui <peterkaiserui@gmail.com>
2016-02-04 11:53:25 -0800

    [style]: css for logout

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -8,7 +8,7 @@ export default class Navbar extends React.Component {
    }
 
   render() {
-    let signout = this.props.user.token !== '' ? <li onClick={this.handleSignout.bind(this)}>Sign out</li> : '';
+    let signout = this.props.user.token !== '' ? <li onClick={this.handleSignout.bind(this)}><a>Sign out</a></li> : '';
     let icon = (<span><img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" /></span>);
     return (
       <nav className="navbar navbar-default navBrowse">
