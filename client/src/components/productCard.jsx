590a38824ecb4a6fa44ef843345f70f488d1beeb
seanagibson <seanandgibson@gmail.com>
2016-02-05 19:33:07 -0800

    [style]: changes the number of product cards/row

--- client/src/components/productCard.jsx
+++ client/src/components/productCard.jsx
@@ -10,7 +10,7 @@ import ProductDetailModal from './productDetailModal';
 class ProductCard extends React.Component {
   render() {
     return (
-        <Col xs={6} md={3} className="portfolio-item">
+        <Col xs={12} sm={6} md={4} className="portfolio-item">
           <div className="productCard" id={this.props.productDetail._id}>
             <div className="productName">
                {this.props.productDetail.productName}
