828954caaeeed64aebd59198ee5af0a5749ed4ca
seanagibson <seanandgibson@gmail.com>
2016-02-08 09:38:04 -0800

    [style]: fixes product card css

--- client/src/components/productList.jsx
+++ client/src/components/productList.jsx
@@ -12,14 +12,14 @@ export default class ProductList extends React.Component{
   }
 
   renderProductCards(){
-    if(this.state.searchVal.length > 0){ 
+    if(this.state.searchVal.length > 0){
       var matchingItems = [];
       var searchTerm = this.state.searchVal.trim().toLowerCase();
         this.props.products.filter(function(product){
           var checkVal = product.productName.trim().toLowerCase();
           if(checkVal.indexOf(searchTerm) !== -1){
               matchingItems.push(product);
-            }     
+            }
         });
       return matchingItems.map((product, index) => {
         return <ProductCard key={index} productDetail={product} resetBuyMsg={this.props.resetBuyMsg}/>
@@ -38,7 +38,7 @@ export default class ProductList extends React.Component{
       <div className="col-sm-12 col-md-12 input-lg">
        <div>
          <input className="col-md-offset-3 col-md-6 input-lg" type="text"
-          placeholder="Search for..." 
+          placeholder="Search for..."
           value = {this.state.searchVal}
           onChange={(event) => this.onChangeFunc(event.target.value)} />
               <span className="input-group-btn">
