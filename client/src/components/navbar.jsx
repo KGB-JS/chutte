5adcdd1ccd0c93b58bb8b6c2c6cafb264559b68a
seanagibson <seanandgibson@gmail.com>
2016-03-14 19:01:34 -1000

    [refactor]: adds active links to sidebar and nav

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -14,18 +14,18 @@ export default class DefaultNavbar extends React.Component {
     let icon = (<span><img className="logo" src="../assets/chutteLogo.png"  alt="Chutte Logo" height="50" width="50" /></span>);
     if(this.props.user.token !== ''){
       return (
-        <nav className="navbar navbar-default navBrowse">  
+        <nav className="navbar navbar-default navBrowse">
           <div className="container-fluid chutteGreen">
               <div className="navbar-header">
                 <Link to="/" className="navbar-brand">{icon} Chutte</Link>
-              </div> 
-            <ul className="nav navbar-nav navbar-right"> 
-              <li><Link to="/browse"><span className="glyphicon glyphicon-shopping-cart"></span> Browse</Link></li>
-              <li><Link to="/dashboard/profile"><span className="glyphicon glyphicon-user"></span> Dashboard</Link></li>            
-              <li><a onClick={this.handleSignout.bind(this)}><span className="glyphicon glyphicon-log-out"></span> Sign out</a></li>
-            </ul>          
+              </div>
+            <ul className="nav navbar-nav navbar-right">
+              <li><Link to="/browse" activeClassName="active"><span className="glyphicon glyphicon-shopping-cart"></span> Browse</Link></li>
+              <li><Link to="/dashboard/profile" activeClassName="active"><span className="glyphicon glyphicon-user"></span> Dashboard</Link></li>
+              <li><a onClick={this.handleSignout.bind(this)} activeClassName="active"><span className="glyphicon glyphicon-log-out"></span> Sign out</a></li>
+            </ul>
           </div>
-        </nav> 
+        </nav>
       );
     } else {
       return (
@@ -72,7 +72,7 @@ export default class DefaultNavbar extends React.Component {
 //     <div class="collapse navbar-collapse navbar-right" id="navbar-collapse-1">
 //       <ul class="nav navbar-nav">
 //         <li><Link to="/browse"><span className="glyphicon glyphicon-shopping-cart"></span> Browse</Link></li>
-//         <li><Link to="/dashboard/profile"><span className="glyphicon glyphicon-user"></span> Dashboard</Link></li>            
+//         <li><Link to="/dashboard/profile"><span className="glyphicon glyphicon-user"></span> Dashboard</Link></li>
 //         <li><a onClick={this.handleSignout.bind(this)}><span className="glyphicon glyphicon-signout"></span> Sign out</a></li>
 //       </ul>
 //     </div>
